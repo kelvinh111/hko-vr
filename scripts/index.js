2df320a6fba833f33b7513bda571d5125cc9224f
kelvinh111 <kelvinhung.register@gmail.com>
2020-09-08 15:22:21 +0800

    yeah

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+pannellum.viewer("panorama", {
+  type: "equirectangular",
+  autoLoad: true,
+  friction: 0.1,
+  panorama: "https://public.kelvinh.studio/o/pano3.jpg",
+  pitch: 9.69227423602026,
+  yaw: 118.76556380799649,
+  //hotSpotDebug: true,
+  hotSpots: [
+    {
+      pitch: 8.270364771249618,
+      yaw: 72.6065876382174,
+      type: "info",
+      scale: true,
+      text: "Nuclear Fission",
+      URL: "http://www.weather.gov.hk/tc/index.html"
+    },
+    {
+      pitch: -3.709476281587014,
+      yaw: 81.03609798193251,
+      type: "info",
+      scale: true,
+      text: "Chain Reaction",
+      URL: "http://www.weather.gov.hk/tc/index.html"
+    },
+    {
+      pitch: 8.525246627903405,
+      yaw: 161.76762202947907,
+      type: "info",
+      scale: true,
+      text: "Human Body",
+      URL: "http://www.weather.gov.hk/tc/index.html"
+    }
+  ]
+});
